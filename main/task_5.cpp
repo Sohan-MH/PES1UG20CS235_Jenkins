26fa7e9eb64ac1543980b16d378793bc6766e0a8
Sohan M H <sohan.mh12@gmail.com>
2023-02-16 09:12:08 +0530

    tcommit

---
+++ main/task_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"\n";
+  cout << "Jenkins Pipeline\n";
+  cout << "PES1UG20CS235\n";
+  cout<<"This is a Task 5.\n";
+  cout << "Task 5\n";
+  cout << "I have successfully built and run a pipeline\n";
+  return 0;
+}
